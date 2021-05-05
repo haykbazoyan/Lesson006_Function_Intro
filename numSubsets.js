d0cba04c90efedac1f9220b96ce7b7f7cebf2fc3
Hayk Bazoyan <bazoyan13@gmail.com>
2021-05-05 13:15:49 +0400

    feat: code of find all combinations from arr numbers

---
+++ numSubsets.js
@@ -0,0 +1,19 @@
+function numSubsets(numArr) {
+  let result = [];
+
+  for (let i = 0; i < numArr.length - 2; i++) {
+    for (let j = i + 1; j < numArr.length - 1; j++) {
+      for (let k = j + 1; k < numArr.length; k++) {
+        let thirdLoopArr = [];
+        thirdLoopArr.push(numArr[i]);
+        thirdLoopArr.push(numArr[j]);
+        thirdLoopArr.push(numArr[k]);
+        result.push(thirdLoopArr);
+      }
+    }
+  }
+  return result;
+}
+
+let arr = [5, 9, 4, 11];
+console.log(numSubsets(arr));
